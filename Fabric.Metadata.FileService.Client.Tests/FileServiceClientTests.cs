c4c5c7ddc99b48e053a582121ef095bf5559edec
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-08-28 17:43:36 -0700

    add a skeleton unit test project

---
+++ Fabric.Metadata.FileService.Client.Tests/FileServiceClientTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fabric.Metadata.FileService.Client.Tests
+{
+    [TestClass]
+    public class FileServiceClientTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Fabric.Metadata.FileService.Client.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fabric.Metadata.FileService.Client.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fabric.Metadata.FileService.Client.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6094b73c-cb7b-46d4-b5d1-9d48d05888bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
